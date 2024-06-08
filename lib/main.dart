5afb72056e78de90cf32fa40566ea213c2647307
nazarii <nazarii.honcharenko@haven.com>
2024-06-08 11:47:14 +0300

    Add google fonts license in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:construction_assistant_app/app/app_component.dart';
 import 'package:construction_assistant_app/l10n/generated/app_localizations.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const ConstructionAssistantApp());
+void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  runApp(const ConstructionAssistantApp());
+}
 
 class ConstructionAssistantApp extends StatelessWidget {
   const ConstructionAssistantApp({super.key});
