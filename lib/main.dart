3a234b66b99e421ae7966a11ed84c0c274a08359
nazarii <nazarii.honcharenko@haven.com>
2024-06-08 11:31:18 +0300

    Add app_en.arb

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:construction_assistant_app/app/app_component.dart';
+import 'package:construction_assistant_app/l10n/generated/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const ConstructionAssistantApp());
@@ -8,7 +9,9 @@ class ConstructionAssistantApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
+        title: AppLocalizations.of(context).constructionAssistantAppTitle,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
