33dcba2259e6d4314a01d28e087281c2e6295309
nazarii <nazarii.honcharenko@haven.com>
2024-06-08 12:06:12 +0300

    Add hide_keyboard_component.dart

---
+++ lib/app/utils/component/hide_keyboard_component.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HideKeyboardComponent extends StatelessWidget {
+  const HideKeyboardComponent({
+    super.key,
+    required Widget child,
+  }) : _child = child;
+
+  final Widget _child;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: FocusManager.instance.primaryFocus?.unfocus,
+        child: _child,
+      );
+}
