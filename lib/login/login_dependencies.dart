164ca346450a2607719336bf251894abc98a048d
nazarii <nazarii.honcharenko@haven.com>
2024-06-08 13:55:34 +0300

    Add login_store.dart

---
+++ lib/login/login_dependencies.dart
@@ -0,0 +1,35 @@
+import 'package:construction_assistant_app/app/app_dependencies.dart';
+import 'package:construction_assistant_app/app/store/app_store.dart';
+import 'package:construction_assistant_app/app/utils/component/dependency_scope.dart';
+import 'package:construction_assistant_app/app/utils/notifier/app_navigation_state_notifier.dart';
+import 'package:construction_assistant_app/login/store/login_store.dart';
+import 'package:construction_assistant_app/login/utils/formatter/login_error_formatter.dart';
+import 'package:construction_assistant_app/login/utils/use_case/login_use_case.dart';
+import 'package:construction_assistant_app/login/utils/validation/login_validator.dart';
+import 'package:flutter/material.dart';
+
+class LoginDependencies extends StatefulWidget {
+  const LoginDependencies({
+    super.key,
+    required Widget child,
+  }) : _child = child;
+
+  final Widget _child;
+
+  @override
+  State<LoginDependencies> createState() => _LoginDependenciesState();
+}
+
+class _LoginDependenciesState extends State<LoginDependencies> with DependencyScope {
+  @override
+  Widget build(BuildContext context) => widget._child;
+
+  @override
+  void registerDependencies() {
+    getIt.registerLazySingleton<LoginUseCase>(() => LoginUseCase());
+    getIt.registerLazySingleton<LoginValidator>(() => LoginValidator());
+    getIt.registerLazySingleton<LoginErrorFormatter>(() => LoginErrorFormatter());
+    getIt.registerLazySingleton<AppNavigationStateNotifier>(() => getIt<AppStore>());
+    getIt.registerLazySingleton<LoginStore>(() => LoginStore());
+  }
+}
---
+++ lib/login/store/login_store.dart
@@ -0,0 +1,82 @@
+import 'package:construction_assistant_app/app/app_dependencies.dart';
+import 'package:construction_assistant_app/app/utils/notifier/app_navigation_state_notifier.dart';
+import 'package:construction_assistant_app/login/utils/entity/login_input_state.dart';
+import 'package:construction_assistant_app/login/utils/formatter/login_error_formatter.dart';
+import 'package:construction_assistant_app/login/utils/use_case/login_use_case.dart';
+import 'package:construction_assistant_app/login/utils/validation/login_validation.dart';
+import 'package:construction_assistant_app/login/utils/validation/login_validator.dart';
+import 'package:mobx/mobx.dart';
+
+part 'login_store.g.dart';
+
+class LoginStore = _LoginStore with _$LoginStore;
+
+abstract class _LoginStore with Store {
+  final LoginUseCase _loginUseCase = getIt<LoginUseCase>();
+  final LoginValidator _loginValidator = getIt<LoginValidator>();
+  final LoginErrorFormatter _loginErrorFormatter = getIt<LoginErrorFormatter>();
+  final AppNavigationStateNotifier _appNavigationStateNotifier = getIt<AppNavigationStateNotifier>();
+
+  @readonly
+  LoginInputState _loginState = LoginInputState.empty();
+  @readonly
+  LoginValidation _loginValidation = LoginValidation.empty();
+  @readonly
+  bool _obscurePasswordField = true;
+  @readonly
+  bool _shouldNavigateToCreateAccount = false;
+  @readonly
+  bool _isLoading = false;
+  @readonly
+  String? _errorMessage;
+
+  @computed
+  bool get shouldEnableContinueButton => _loginValidation.isValid;
+
+  @action
+  void toggleObscurePasswordField({required String controllerText}) {
+    if (controllerText.isNotEmpty) {
+      _obscurePasswordField = !_obscurePasswordField;
+    }
+  }
+
+  @action
+  void updateLoginState({
+    String? email,
+    String? password,
+  }) {
+    _loginState = _loginState.copyWith(
+      emailText: email,
+      passwordText: password,
+    );
+    _validateLoginState();
+  }
+
+  void _validateLoginState() => _loginValidation = _loginValidator.validateLogin(loginState: _loginState);
+
+  @action
+  Future<void> continueButtonClicked() async {
+    _isLoading = true;
+    try {
+      await _loginUseCase.login(
+        email: _loginState.emailText,
+        password: _loginState.passwordText,
+      );
+      // TODO(naz): save tokens
+      await _appNavigationStateNotifier.updateNavigationState();
+    } catch (error) {
+      _errorMessage = _loginErrorFormatter.formatError(error);
+    } finally {
+      _isLoading = false;
+    }
+  }
+
+  @action
+  void registerButtonClicked() => _shouldNavigateToCreateAccount = true;
+
+  @action
+  void handleShouldNavigateToCreateAccount() => _shouldNavigateToCreateAccount = false;
+
+  @action
+  void handleErrorMessage() => _errorMessage = null;
+}
