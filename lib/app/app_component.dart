93794a5a70496af7a6b2979f88a8bdd4dc7ce967
nazarii <nazarii.honcharenko@haven.com>
2024-06-08 11:13:38 +0300

    Initial project formatting

---
+++ lib/app/app_component.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppComponent extends StatelessWidget {
+  const AppComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Placeholder();
+}
