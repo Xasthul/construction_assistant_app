68939386a7655f9de1ce08156724009ea256fd29
nazarii <nazarii.honcharenko@haven.com>
2024-06-08 13:36:06 +0300

    Add app_navigation_state_notifier.dart

--- lib/app/store/app_store.dart
+++ lib/app/store/app_store.dart
@@ -1,11 +1,15 @@
 import 'package:construction_assistant_app/app/utils/entity/app_navigation_state.dart';
+import 'package:construction_assistant_app/app/utils/notifier/app_navigation_state_notifier.dart';
 import 'package:mobx/mobx.dart';
 
 part 'app_store.g.dart';
 
 class AppStore = _AppStoreBase with _$AppStore;
 
-abstract class _AppStoreBase with Store {
+abstract class _AppStoreBase with Store, AppNavigationStateNotifier {
   @readonly
   AppNavigationState _navigationState = AppNavigationState.login;
+
+  @override
+  Future<void> updateNavigationState() async {}
 }
---
+++ lib/app/utils/notifier/app_navigation_state_notifier.dart
@@ -0,0 +1,3 @@
+mixin AppNavigationStateNotifier {
+  Future<void> updateNavigationState();
+}
